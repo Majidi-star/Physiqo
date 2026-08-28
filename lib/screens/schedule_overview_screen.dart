c11ec69da602840cbf8caa718ee15869bb9d42f2
Amirhosein Majidi <tsp10majidi@gmail.com>
2026-08-28 12:06:33 +0330

    Fix missing shamsi_date import in ScheduleOverviewScreen

--- lib/screens/schedule_overview_screen.dart
+++ lib/screens/schedule_overview_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:shamsi_date/shamsi_date.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import '../models/workout_day.dart';
